861fc5dbf1fa6aef6ba0d8d41b67cfb710a5e3a5
KAYOUNG KIM <lvyest.dev@gmail.com>
2026-04-23 19:36:01 +0900

    [chore] prisma 초기 세팅 (uuid v7 미들웨어, schema 작성)

---
+++ apps/backend/src/common/config/prisma.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from '@prisma/client';
+import { uuidv7 } from 'uuidv7';
+
+// 클라이언트 생성
+// dev 환경에서 Hot Reload 시 PrismaClient 중복 생성 방지
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient;
+};
+
+const basePrisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = basePrisma;
+}
+
+// 익스텐션(미들웨어) 정의
+const prisma = basePrisma.$extends({
+  query: {
+    $allModels: {
+      async create({ args, query }) {
+
+        // id 없을 경우 uuid v7 삽입 
+        const data = args.data as any;
+        if (!data.id) {
+          data.id = uuidv7();
+        }
+        return query(args);
+      },
+    },
+  },
+});
+
+export default prisma;
