557bdfc99d40c7c753e75f62510d760536068faf
s576air <s576ai@naver.com>
2026-04-29 00:51:18 +0900

    [feat] orval 설정

---
+++ apps/frontend/orval.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'orval';
+
+export default defineConfig({
+  api: {
+    input: 'http://localhost:3000/openapi.json',
+    output: {
+      target: './src/api/generated.ts',
+      client: 'axios',
+    },
+  },
+});
